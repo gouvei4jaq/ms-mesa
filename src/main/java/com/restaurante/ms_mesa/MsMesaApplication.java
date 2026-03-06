ba6b796bf1b3a6cda6d374b3168872e4e16162f0
gouvei4jaq <jaquelinegouvei4@gmail.com>
2026-03-05 21:44:05 -0300

    add configurações inciais

---
+++ src/main/java/com/restaurante/ms_mesa/MsMesaApplication.java
@@ -0,0 +1,13 @@
+package com.restaurante.ms_mesa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsMesaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsMesaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/restaurante/ms_mesa/MsMesaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurante.ms_mesa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsMesaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
