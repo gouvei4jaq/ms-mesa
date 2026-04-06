4b7d11064bee07d7537713eef738cad482d50a3d
gouvei4jaq <jaquelinegouvei4@gmail.com>
2026-04-06 18:41:24 -0300

    Criação do MesaRepository e MesaEntity

--- src/main/java/com/restaurante/ms_mesa/entity/MesaEntity.java
+++ src/main/java/com/restaurante/ms_mesa/entity/MesaEntity.java
@@ -1,20 +1,41 @@
 package com.restaurante.ms_mesa.entity;
 
-import jakarta.persistence.*;
+import com.restaurante.ms_mesa.enumeration.StatusMesa;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Data
 @Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "mesa")
 public class MesaEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
     private UUID id;
+
     @Column(name = "numero")
-    private Integer number;
-    private String status;
+    private Integer numero;
+
+    @Column
+    private StatusMesa status;
+
+    @Column
+    private Integer capacidade;
+
     @Column(name = "data_de_atualizacao")
-    private Timestamp atualizationDate;
+    private LocalDateTime dataDeAtualizacao;
 
 }
--- src/main/java/com/restaurante/ms_mesa/repository/MesaRepository.java
+++ src/main/java/com/restaurante/ms_mesa/repository/MesaRepository.java
@@ -4,8 +4,10 @@ import com.restaurante.ms_mesa.entity.MesaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface MesaRepository extends JpaRepository<MesaEntity, UUID> {
+    Optional<MesaEntity> findByNumero(int numero);
 }
