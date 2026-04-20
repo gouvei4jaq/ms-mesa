803dd824ea5cae8150a90ecc76c107367c543709
gouvei4jaq <jaquelinegouvei4@gmail.com>
2026-04-20 17:04:38 -0300

    add anotação

--- src/main/java/com/restaurante/ms_mesa/response/MesaResponse.java
+++ src/main/java/com/restaurante/ms_mesa/response/MesaResponse.java
@@ -1,9 +1,10 @@
 package com.restaurante.ms_mesa.response;
 
 import com.restaurante.ms_mesa.enumeration.StatusMesa;
+import lombok.Builder;
 
 import java.util.UUID;
-
+@Builder
 public class MesaResponse {
     private UUID id;
     private Integer numero;
