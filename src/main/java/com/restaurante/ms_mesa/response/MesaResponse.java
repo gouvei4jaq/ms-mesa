77b378ef693444dbebdb61a4ae2e9154ea88aa94
gouvei4jaq <jaquelinegouvei4@gmail.com>
2026-04-21 16:07:44 -0300

    add getter e setter

--- src/main/java/com/restaurante/ms_mesa/response/MesaResponse.java
+++ src/main/java/com/restaurante/ms_mesa/response/MesaResponse.java
@@ -2,8 +2,13 @@ package com.restaurante.ms_mesa.response;
 
 import com.restaurante.ms_mesa.enumeration.StatusMesa;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.UUID;
+
+@Getter
+@Setter
 @Builder
 public class MesaResponse {
     private UUID id;
