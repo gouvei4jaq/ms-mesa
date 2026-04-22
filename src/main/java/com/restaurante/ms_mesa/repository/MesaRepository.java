76e2d1aaf062fa209a5b8c6237e8cb0613b67324
gouvei4jaq <jaquelinegouvei4@gmail.com>
2026-04-22 13:58:57 -0300

    add métodos para buscas

--- src/main/java/com/restaurante/ms_mesa/repository/MesaRepository.java
+++ src/main/java/com/restaurante/ms_mesa/repository/MesaRepository.java
@@ -1,13 +1,21 @@
 package com.restaurante.ms_mesa.repository;
 
 import com.restaurante.ms_mesa.entity.MesaEntity;
+import com.restaurante.ms_mesa.enumeration.StatusMesa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface MesaRepository extends JpaRepository<MesaEntity, UUID> {
     Optional<MesaEntity> findByNumero(int numero);
+
+    List<MesaEntity> findByStatusAndCapacidade(StatusMesa status, int capacidade);
+
+    List<MesaEntity> findByStatus(StatusMesa status);
+
+    List<MesaEntity> findByCapacidade(Integer capacidade);
 }
