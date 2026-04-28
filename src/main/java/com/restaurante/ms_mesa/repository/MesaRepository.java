f6d9bb74a5c500698c60c31617c4071d3f8919aa
gouvei4jaq <jaquelinegouvei4@gmail.com>
2026-04-28 10:49:17 -0300

    refatoração nos metodos de fitlragem

--- src/main/java/com/restaurante/ms_mesa/repository/MesaRepository.java
+++ src/main/java/com/restaurante/ms_mesa/repository/MesaRepository.java
@@ -3,6 +3,8 @@ package com.restaurante.ms_mesa.repository;
 import com.restaurante.ms_mesa.entity.MesaEntity;
 import com.restaurante.ms_mesa.enumeration.StatusMesa;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,9 +15,13 @@ import java.util.UUID;
 public interface MesaRepository extends JpaRepository<MesaEntity, UUID> {
     Optional<MesaEntity> findByNumero(int numero);
 
-    List<MesaEntity> findByStatusAndCapacidade(StatusMesa status, int capacidade);
+    @Query("""
+    SELECT m FROM MesaEntity m
+    WHERE (:status IS NULL OR m.status = :status)
+    AND (:capacidade IS NULL OR m.capacidade = :capacidade)
+    """)
+    List<MesaEntity> findByFilters(@Param("status") StatusMesa status,
+                                   @Param("capacidade") Integer capacidade);
 
-    List<MesaEntity> findByStatus(StatusMesa status);
 
-    List<MesaEntity> findByCapacidade(Integer capacidade);
 }
